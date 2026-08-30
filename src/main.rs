3d9b22d7056df88491b7fc01afd8c3cc04fdf456
Tore Aurstad <tore.aurstad@gmail.com>
2026-08-30 23:33:31 +0200

    Added lastordefault trait

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
 
     let v = vec![- 30, 10, 20, 30, 120];
     let a = v.first_or_default();
-
     println!("The first item of vector v is: {}", a);
+
+    let b = v.last_or_default();
+    println!("The last item of vector v is: {}", b);
 }
 
 
-/* FirstOrDefault trait */
+/// Gets the first item or the default value.
 pub trait FirstOrDefault {
     type Item;
+
+    /// Returns the first item or the default value.
     fn first_or_default(&self) -> Self::Item;
 }
 
-impl<T : Default + Clone> FirstOrDefault for [T] {
+impl<T> FirstOrDefault for [T] 
+where
+    T : Default + Clone
+{
     type Item = T;
 
     fn first_or_default(&self) -> T {
@@ -22,7 +29,22 @@ impl<T : Default + Clone> FirstOrDefault for [T] {
     }
 }
 
-/* LastOrDefault trait */
+/// Gets the last item or the default value.
+pub trait LastOrDefault {
+    type Item;
 
+    /// Returns the last item or the default value.
+    fn last_or_default(&self) -> Self::Item;
+}
 
+impl<T> LastOrDefault for [T]
+where 
+    T : Default + Clone
+{
+    type Item = T; 
+
+    fn last_or_default(&self) -> T {
+        self.last().cloned().unwrap_or_default()
+    }
+}
 
